75352b234209e995420ce04cd90b0f9840cf8ff5
Muhammad-Ahmad618 <muhammad.ahmad.86.18@gmail.com>
2025-11-24 21:38:37 +0500

    feat: Add initial GameDetailsPage component with basic layout for title, rating, and genre.

---
+++ src/Screens/GameDetailsPage/GameDetailsPage.jsx
@@ -0,0 +1,28 @@
+
+export default function GameDetailsPage() {
+
+    return (
+
+        <div>
+
+            <div>
+               <p>BreadCrumbs</p>
+               <h1>Title</h1>
+               <div>
+                <div>
+                <p>rating</p>
+                </div>
+                <div>
+                    <p>genre</p>
+                </div>
+               </div>
+            </div>
+
+
+        </div>
+
+
+    )
+
+
+}
